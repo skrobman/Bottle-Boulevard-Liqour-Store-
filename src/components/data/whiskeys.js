61e92d2563e8d064e5816a7085ff9f7f21596aab
skrobman1010 <mikhailskrobat@gmail.com>
2024-05-30 03:14:36 +0200

    Updated

--- src/components/data/whiskeys.js
+++ src/components/data/whiskeys.js
@@ -7,7 +7,8 @@ export const data = {
             "price": 30.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/products/image_fcbb5b86-e027-486c-8c75-698a320702d5_1024x.jpg?v=1625730561",
-            "best_selling": 7
+            "best_selling": 7,
+            "desc": "A classic choice from Johnie Walker, the Red Label boasts a smooth taste with hints of spice and vanilla, making it perfect for cocktails or enjoying neat."
         },
         {
             "id": 2,
@@ -16,7 +17,8 @@ export const data = {
             "price": 45.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/products/image_eb0f4931-c085-4f7c-9490-d23cb4ef8410_1024x.jpg?v=1673568705",
-            "best_selling": 14
+            "best_selling": 14,
+            "desc": "Renowned for its depth and complexity, the Black Label offers a rich blend of smoky and fruity flavors, with a lingering finish that appeals to whiskey enthusiasts worldwide."
         },
         {
             "id": 3,
@@ -25,7 +27,8 @@ export const data = {
             "price": 55.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/products/Johnnie-Walker-Double-Black-Scotch-Whisky_1024x.jpg?v=1621823824",
-            "best_selling": 5
+            "best_selling": 5,
+            "desc": "Building upon the iconic Black Label, the Double Black Label intensifies the smoky notes and adds layers of spice and sweetness, creating a bold and memorable drinking experience."
         },
         {
             "id": 4,
@@ -34,7 +37,8 @@ export const data = {
             "price": 199.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/products/image_7d16f7ae-0423-4326-aee5-fd6cb140589c_1024x.jpg?v=1677962235",
-            "best_selling": 22
+            "best_selling": 22,
+            "desc": "The epitome of luxury, Johnie Walker Blue Label is a masterful blend of rare whiskies, delivering a harmonious balance of rich fruitiness, gentle smokiness, and velvety smoothness that defines sophistication."
         },
         {
             "id": 5,
@@ -43,7 +47,8 @@ export const data = {
             "price": 27.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/products/Jack-Daniels-Old-No.-7-Tennessee-Whiskey_1024x.jpg?v=1621823790",
-            "best_selling": 20
+            "best_selling": 20,
+            "desc": "Jack Daniels Old No. 7 is a true American classic, characterized by its mellow flavor profile with hints of caramel and oak, making it a timeless choice for whiskey aficionados."
         },
         {
             "id": 6,
@@ -52,7 +57,8 @@ export const data = {
             "price": 32.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/files/image_9b7d6575-9ffd-4dd9-953d-3e9ed7bf1b05_1024x.jpg?v=1682723704",
-            "best_selling": 21
+            "best_selling": 21,
+            "desc": "Infused with natural honey flavors, Jack Daniels Tennessee Honey offers a uniquely smooth and sweet taste, perfect for sipping on the rocks or as a versatile ingredient in cocktails."
         },
         {
             "id": 7,
@@ -61,7 +67,8 @@ export const data = {
             "price": 49.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/products/image_029b8321-f0ae-4371-b833-2f6e6a8e1e4c_1024x.jpg?v=1669587088",
-            "best_selling": 8
+            "best_selling": 8,
+            "desc": "Each bottle of Jack Daniels Single Barrel Select is hand-selected for its exceptional quality, boasting rich caramel and toasted oak flavors with a lingering, warm finish that exemplifies craftsmanship."
         },
         {
             "id": 8,
@@ -70,7 +77,8 @@ export const data = {
             "price": 39.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/products/image_5ce54874-622b-4a3c-abbe-487defe93799_1024x.jpg?v=1672723723",
-            "best_selling": 10
+            "best_selling": 10,
+            "desc": "Distinctively smooth and balanced, Gentleman Jack undergoes a double charcoal mellowing process, resulting in a refined whiskey with notes of caramel, vanilla, and a hint of spice."
         },
         {
             "id": 9,
@@ -79,7 +87,8 @@ export const data = {
             "price": 22.99,
             "bottle_size": "750ml",
             "image": "https://swiat-whisky.sklep.pl/4174-large_default/jim-beam-bourbon-white-label-40-07l.jpg",
-            "best_selling": 15
+            "best_selling": 15,
+            "desc": "A cornerstone of American whiskey, Jim Beam White Label is known for its classic bourbon character, featuring sweet corn, vanilla, and oak flavors that create a versatile and approachable spirit."
         },
         {
             "id": 10,
@@ -88,7 +97,8 @@ export const data = {
             "price": 29.99,
             "bottle_size": "750ml",
             "image": "https://krest.pl/environment/cache/images/500_500_productGfx_3508/jim-beam-black.jpg",
-            "best_selling": 2
+            "best_selling": 2,
+            "desc": "Aged longer for a bolder taste, Jim Beam Black Label offers a rich and full-bodied bourbon experience with deep caramel and oak flavors, making it a favorite among whiskey connoisseurs."
         },
         {
             "id": 11,
@@ -97,7 +107,8 @@ export const data = {
             "price": 34.99,
             "bottle_size": "750ml",
             "image": "https://i.pinimg.com/564x/89/4b/24/894b24b319666a9979d1538a41a1c3cf.jpg",
-            "best_selling": 6
+            "best_selling": 6,
+            "desc": "Double matured in charred oak barrels, Jim Beam Double Oak exhibits a robust flavor profile with layers of caramel, toffee, and a hint of smokiness, resulting in a velvety smooth whiskey."
         },
         {
             "id": 12,
@@ -106,7 +117,8 @@ export const data = {
             "price": 31.99,
             "bottle_size": "750ml",
             "image": "https://i.pinimg.com/564x/cf/59/c0/cf59c08ae577c4c210302f2797168c9d.jpg",
-            "best_selling": 16
+            "best_selling": 16,
+            "desc": "Harnessing the intense flavors trapped within the barrel, Jim Beam Devil's Cut is a bold and robust bourbon with deep oak, spice, and char notes that create a memorable drinking experience."
         },
         {
             "id": 13,
@@ -115,7 +127,8 @@ export const data = {
             "price": 29.99,
             "bottle_size": "750ml",
             "image": "https://i.pinimg.com/564x/34/1d/32/341d325b810d279b5fe18fc1d8ee1d39.jpg",
-            "best_selling": 13
+            "best_selling": 13,
+            "desc": "Jameson Irish Whiskey is a timeless classic known for its smooth and mellow character, featuring delicate notes of honey, vanilla, and toasted wood that reflect the spirit of Ireland."
         },
         {
             "id": 14,
@@ -124,7 +137,8 @@ export const data = {
             "price": 39.99,
             "bottle_size": "750ml",
             "image": "https://i.pinimg.com/564x/e2/6b/f6/e26bf6acc5e7633456ff9eeaf7022936.jpg",
-            "best_selling": 3
+            "best_selling": 3,
+            "desc": "A step above the classic Jameson, Black Barrel is aged in double-charred barrels, imparting rich caramel, spice, and nutty flavors, resulting in a complex and satisfying whiskey."
         },
         {
             "id": 15,
@@ -133,7 +147,8 @@ export const data = {
             "price": 34.99,
             "bottle_size": "750ml",
             "image": "https://i.pinimg.com/564x/38/0e/9c/380e9c017a7d1611fb1430d141d82ba3.jpg",
-            "best_selling": 24
+            "best_selling": 24,
+            "desc": "Finished in stout-seasoned barrels, Jameson Caskmates Stout Edition offers a unique blend of rich cocoa, coffee, and caramel notes, creating a smooth and flavorful whiskey with a distinct character."
         },
         {
             "id": 16,
@@ -142,7 +157,8 @@ export const data = {
             "price": 34.99,
             "bottle_size": "750ml",
             "image": "https://i.pinimg.com/564x/34/61/0f/34610f47e1b490f831348dd18ed43615.jpg",
-            "best_selling": 19
+            "best_selling": 19,
+            "desc": "Finished in IPA beer barrels, Jameson Caskmates IPA Edition boasts vibrant citrus, floral, and hoppy aromas, complemented by sweet malt and toasted oak flavors for a refreshing twist on traditional Irish whiskey."
         },
         {
             "id": 17,
@@ -151,7 +167,8 @@ export const data = {
             "price": 27.99,
             "bottle_size": "750ml",
             "image": "https://i.pinimg.com/564x/af/9a/0b/af9a0b683e6df2d3ea66cd1fa1096d1f.jpg",
-            "best_selling": 1
+            "best_selling": 1,
+            "desc": "Tullamore Dew Original is a triple-distilled blend of pot still, malt, and grain whiskeys, delivering a smooth and gentle flavor profile with hints of citrus, vanilla, and toasted wood that capture the essence of Irish whiskey."
         },
         {
             "id": 18,
@@ -160,7 +177,8 @@ export const data = {
             "price": 39.99,
             "bottle_size": "750ml",
             "image": "https://i.pinimg.com/564x/3a/db/b7/3adbb7b1e4490efb3a107e87161be63e.jpg",
-            "best_selling": 12
+            "best_selling": 12,
+            "desc": "Aged for 12 years, Tullamore Dew 12 Year Old offers a more mature expression with deep oak, spice, and dried fruit flavors, accompanied by a lingering warmth that showcases the artistry of Irish whiskey-making."
         },
         {
             "id": 19,
@@ -169,7 +187,8 @@ export const data = {
             "price": 49.99,
             "bottle_size": "750ml",
             "image": "https://i.pinimg.com/564x/5c/86/83/5c86839a1125fc8e0d8c4967bae62f38.jpg",
-            "best_selling": 11
+            "best_selling": 11,
+            "desc": "Chivas Regal 12 Year Old is a timeless blend of malt and grain whiskies, aged for a minimum of 12 years to achieve a rich and smooth taste with notes of honey, fruit, and oak that define the Chivas house style."
         },
         {
             "id": 20,
@@ -178,7 +197,8 @@ export const data = {
             "price": 69.99,
             "bottle_size": "750ml",
             "image": "https://i.pinimg.com/564x/8e/44/91/8e44916c45acbeb1e6cfabf72bf5b532.jpg",
-            "best_selling": 9
+            "best_selling": 9,
+            "desc": "Crafted with exceptional aged whiskies, Chivas Regal 18 Year Old presents a luxurious drinking experience with layers of rich fruit, chocolate, and spice flavors, culminating in a long and satisfying finish."
         },
         {
             "id": 21,
@@ -187,7 +207,8 @@ export const data = {
             "price": 584.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/files/FullSizeRender_600ac71d-cad6-40e3-afb6-906424ce125e_1024x.jpg?v=1704178743",
-            "best_selling": 18
+            "best_selling": 18,
+            "desc": "Old Carter Batch 6 Bourbon is a limited edition release showcasing the finest hand-selected barrels, offering an indulgent palate of caramel, dark fruit, and baking spices, culminating in a velvety smooth finish that epitomizes premium bourbon."
         },
         {
             "id": 22,
@@ -196,7 +217,8 @@ export const data = {
             "price": 299.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/files/FullSizeRender_2e009a46-0737-4264-af0c-3970053b274b_1024x.jpg?v=1702609732",
-            "best_selling": 23
+            "best_selling": 23,
+            "desc": "With a higher proof, Old Carter Batch 15 delivers a robust and intense flavor profile, featuring bold notes of oak, caramel, and spice, making it a standout choice for whiskey enthusiasts seeking depth and complexity."
         },
         {
             "id": 23,
@@ -205,7 +227,8 @@ export const data = {
             "price": 249.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/files/FullSizeRender_37477fa9-7302-44a3-a1b5-0e5433c0607a_1024x.jpg?v=1704178411",
-            "best_selling": 17
+            "best_selling": 17,
+            "desc": "A testament to quality craftsmanship, Old Carter Straight Rye Whiskey Batch 8 presents a well-balanced blend of rye spices, caramel, and vanilla, underscored by a smooth and lingering finish that exemplifies the art of rye whiskey-making."
         },
         {
             "id": 24,
@@ -214,8 +237,8 @@ export const data = {
             "price": 399.99,
             "bottle_size": "750ml",
             "image": "https://onlineliquor.com/cdn/shop/files/FullSizeRender_9d91e205-6bc3-462a-962c-9bb910205590_1024x.jpg?v=1704178322",
-            "best_selling": 4
+            "best_selling": 4,
+            "desc": "Offering a more intense flavor experience, Old Carter Straight Rye Whiskey Batch 7 boasts rich and complex layers of spice, fruit, and oak, creating a bold and captivating whiskey that demands attention."
         }
     ]
 }
-
--- src/components/items/WhiskeyItemPage.js
+++ src/components/items/WhiskeyItemPage.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { data } from '../data/whiskeys';
 import Header from '../common/Header';
 import Breadcrumb from '../storeSections/Breadcrumb';
 
 const WhiskeyItemPage = () => {
     const { id } = useParams();
-    const whiskeyIndex = data.whiskeyData.findIndex(item => item.id === parseInt(id));
+    const whiskeyIndex = data.whiskeyData.findIndex(item => item.id === parseInt(id, 10));
     const whiskey = data.whiskeyData[whiskeyIndex];
 
     const nextWhiskey = data.whiskeyData[whiskeyIndex + 1];
     const prevWhiskey = data.whiskeyData[whiskeyIndex - 1];
 
-    if (!whiskey) {
-        return <div>Whiskey not found</div>;
+    const [quantity, setQuantity] = useState(0);
+
+    const handleIncrement = () => {
+        setQuantity(quantity + 1);
+    };
+
+    const handleDecrement = () => {
+        if (quantity > 0) {
+            setQuantity(quantity - 1);
+        }
+    };
+
+    if (whiskeyIndex === -1) {
+        return (
+            <>
+                <Header />
+                <div className="flex justify-center mt-48 ml-14 mr-14">
+                    <Breadcrumb />
+                    <div>Whiskey not found</div>
+                </div>
+            </>
+        );
     }
 
     return (
@@ -21,25 +42,42 @@ const WhiskeyItemPage = () => {
             <Header />
             <div className="flex justify-between mt-48 ml-14 mr-14">
                 <Breadcrumb />
-                <small>
-                    {prevWhiskey && (
-                        <Link to={`/whiskey/${prevWhiskey.id}`}>Previous</Link>
+                <div className="nav-links flex">
+                    {prevWhiskey ? (
+                        <Link to={`/whiskey/${prevWhiskey.id}`} className="prev-next-link">Previous</Link>
+                    ) : (
+                        <span className="prev-next-link disabled">Previous</span>
                     )}
-                    |
-                    {nextWhiskey && (
-                        <Link to={`/whiskey/${nextWhiskey.id}`}>Next</Link>
+                    {nextWhiskey ? (
+                        <Link to={`/whiskey/${nextWhiskey.id}`} className="prev-next-link">Next</Link>
+                    ) : (
+                        <span className="prev-next-link disabled">Next</span>
                     )}
-                </small>
+                </div>
             </div>
-            <div className="whiskey-items-page ml-14 mr-14">
-                <div className="left-side-items">
-                    <div className="item-img w-72">
-                        <img src={whiskey.image} alt={whiskey.name} className="w-full h-72 object-contain" />
+            <div className="whiskey-items-page flex ml-14 mr-14 pt-5">
+                <div className="item-img">
+                    <img src={whiskey.image} alt={whiskey.name} className="whiskey-image" />
+                </div>
+                <div className="whiskey-details">
+                    <h2 className="whiskey-name">{whiskey.name}</h2>
+                    <h3 className="whiskey-brand">{whiskey.brand}</h3>
+                    <p className="whiskey-desc">{whiskey.desc}</p>
+                    <p className="whiskey-price">${whiskey.price.toFixed(2)}</p>
+                    <div className="quantity-container">
+                        <button onClick={handleDecrement} className="quantity-button">-</button>
+                        <div className="quantity">{quantity}</div>
+                        <button onClick={handleIncrement} className="quantity-button">+</button>
                     </div>
+                    <button className='add-to-cart'>Add to cart</button>
                 </div>
             </div>
         </>
     );
+};
+
+WhiskeyItemPage.propTypes = {
+    id: PropTypes.string
 };
 
 export default WhiskeyItemPage;
